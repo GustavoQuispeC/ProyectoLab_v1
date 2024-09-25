e2098039999ec8eef41a5740e6e1e2c2e9713513
Gustavo <gusstavocta@gmail.com>
2024-09-24 23:32:41 -0500

    modificando height

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -66,7 +66,7 @@ const Hero = () => {
                           src={slide.image}
                           alt={slide.caption}
                           width={1500}
-                          height={600}
+                          height={500}
                           className="h-full w-full object-contain"
                         />
                       </div>
